0c38e4ba0994f5276da1e37b1b9c5d4b49557cb5
Duvan Roque <dsmith.mr@gmail.com>
2022-11-04 10:53:12 -0500

    Manejo estilos

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,33 +1,34 @@
 import {useState} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
 
 export const ContadorScreen = () => {
   const [contador, setContador] = useState(10);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 40,
-          top: -15,
-        }}>
-        Contador: {contador}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {contador}</Text>
 
       <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View
-          style={{
-            backgroundColor: 'green',
-            borderRadius: 100,
-          }}>
+        <View style={styles.bottonIncrementar}>
           <Text>+1</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15,
+  },
+  bottonIncrementar: {
+    backgroundColor: 'green',
+    borderRadius: 100,
+  },
+});
