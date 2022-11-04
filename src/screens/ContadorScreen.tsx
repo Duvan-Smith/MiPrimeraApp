51e3a173de856d73b4503df35fbfbb37e7c04849
Duvan Roque <dsmith.mr@gmail.com>
2022-11-04 11:04:18 -0500

    Uso TouchableOpacity

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -8,9 +8,19 @@ export const ContadorScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {contador}</Text>
 
-      <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={styles.bottonIncrementar}>
-          <Text>+1</Text>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
+        onPress={() => setContador(contador + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setContador(contador - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -27,8 +37,27 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -15,
   },
-  bottonIncrementar: {
-    backgroundColor: 'green',
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });
