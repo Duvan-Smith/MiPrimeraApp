492ca557c9c5ff8c4cde5a0a3baa56845821d3a6
Duvan Roque <dsmith.mr@gmail.com>
2022-11-05 17:24:59 -0500

    Posicion absoluta

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -3,8 +3,10 @@ import {StyleSheet, View} from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.cagaRoja} />
       <View style={styles.cajaMorada} />
       <View style={styles.cagaNaranja} />
+      <View style={styles.cagaVerde} />
     </View>
   );
 };
@@ -12,11 +14,11 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
   container: {
     // flex: 1,
-    width: 400,
+    width: '100%',
     height: 400,
     backgroundColor: '#28C4D9',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   cajaMorada: {
     width: 100,
@@ -24,8 +26,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856D6',
     borderWidth: 10,
     borderColor: 'white',
-    top: 50,
-    left: 100,
+    position: 'absolute',
+    top: 0,
+    right: 0,
   },
   cagaNaranja: {
     width: 100,
@@ -33,6 +36,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0A23B',
     borderWidth: 10,
     borderColor: 'white',
-    top: -10,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  cagaVerde: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  cagaRoja: {
+    margin: 50,
+    backgroundColor: 'red',
+    borderWidth: 10,
+    borderColor: 'white',
+    ...StyleSheet.absoluteFillObject,
   },
 });
