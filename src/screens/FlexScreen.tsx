94179792a4b659d60cd334fbeec2c1687198914f
Duvan Roque <dsmith.mr@gmail.com>
2022-11-06 08:58:06 -0500

    flex direction

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -14,21 +14,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
+    flexDirection: 'row',
   },
   caja1: {
-    flex: 3, // 3 + 2 + 1 = 6, toma el 0.3 de 6
+    // flex: 3, // 3 + 2 + 1 = 6, toma el 0.3 de 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 20,
   },
   caja2: {
-    flex: 2, // 3 + 2 + 1 = 6, toma el 0.2 de 6
+    // flex: 2, // 3 + 2 + 1 = 6, toma el 0.2 de 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 20,
   },
   caja3: {
-    flex: 1, // 3 + 2 + 1 = 6, toma el 0.1 de 6
+    // flex: 1, // 3 + 2 + 1 = 6, toma el 0.1 de 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 20,
