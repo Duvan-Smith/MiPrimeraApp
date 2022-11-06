bb1715cdc18116d0c5bac49af0a7ae6f7e6d3407
Duvan Roque <dsmith.mr@gmail.com>
2022-11-06 09:13:37 -0500

    Sobre escribir align con alignSelf

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -15,25 +15,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#28C4D9',
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
+    // justifyContent: 'flex-end',
+    // alignItems: 'flex-end',
   },
   caja1: {
     // flex: 3, // 3 + 2 + 1 = 6, toma el 0.3 de 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 20,
+    alignSelf: 'flex-start',
   },
   caja2: {
     // flex: 2, // 3 + 2 + 1 = 6, toma el 0.2 de 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 20,
+    alignSelf: 'center',
   },
   caja3: {
     // flex: 1, // 3 + 2 + 1 = 6, toma el 0.1 de 6
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 20,
+    alignSelf: 'flex-end',
   },
 });
