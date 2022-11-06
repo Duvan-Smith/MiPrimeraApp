a6e415da289b76e97af02e3fbbc0b98723f27902
Duvan Roque <dsmith.mr@gmail.com>
2022-11-06 09:24:25 -0500

    Tarea

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import {SafeAreaView} from 'react-native';
-import {FlexScreen} from './src/screens/FlexScreen';
+import {TareaScreen} from './src/screens/TareaScreen';
 
 const App = () => {
   return (
     // <SafeAreaView style={{height: 400, width: 300}}>
-    <SafeAreaView style={{flex: 1}}>
-      <FlexScreen />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#28425B', //Se agrega para que sea visible el color en pantalla cortada
+      }}>
+      <TareaScreen />
     </SafeAreaView>
   );
 };
