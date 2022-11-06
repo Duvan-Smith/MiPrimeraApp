e7e2e1cee25dbf9a936a5e4aa57a227fb6da90ff
Duvan Roque <dsmith.mr@gmail.com>
2022-11-06 09:35:10 -0500

    Solucion 3

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#5856D6',
+    alignSelf: 'flex-end',
   },
   cajaNaranja: {
     width: 100,
@@ -31,10 +32,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0A23B',
   },
   cajaAzul: {
-    width: '100%',
+    width: 100,
     height: 100,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#28C4D9',
+    alignSelf: 'center',
   },
 });
